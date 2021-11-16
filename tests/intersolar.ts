5e3ce02e48bd892d962b0916b2ad1b0b2732b3fc
Bort <>
2021-11-16 21:23:10 +0100

    More test refactoring

--- tests/intersolar.ts
+++ tests/intersolar.ts
@@ -6,6 +6,8 @@ import { Intersolar } from '../target/types/intersolar';
 
 const PREFIX = "intersolar"
 
+const intersolarProgram = anchor.workspace.Intersolar as anchor.Program<Intersolar>;
+
 interface MintSetup {
   payerKeypair: Keypair,
   mint: splToken.Token,
@@ -58,35 +60,43 @@ async function setupMint(connection: Connection): Promise<MintSetup> {
   };
 }
 
+interface IntersolarSetup extends MintSetup{
+  intersolarPublicKey,
+  bump
+}
+
+async function setupIntersolar(connection: Connection): Promise<IntersolarSetup> {
+  const setup = await setupMint(connection);
+  const [intersolarPublicKey, bump] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from(PREFIX), setup.mint.publicKey.toBuffer()],
+    intersolarProgram.programId
+  );
+
+  await intersolarProgram.rpc.initialize(
+    bump,
+    {
+      accounts: {
+        intersolar: intersolarPublicKey,
+        user: setup.receiverKeypair.publicKey,
+        tokenMint: setup.mint.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers: [
+        setup.receiverKeypair,
+      ]
+    });
+    return {
+      ...setup,
+      intersolarPublicKey,
+      bump,
+    };
+}
+
 describe('intersolar', () => {
 
   it('initialize should succeed', async () => {
-
-    // Configure the client to use the local cluster.
     anchor.setProvider(anchor.Provider.env());
-
     const connection = anchor.Provider.env().connection;
-
-    const setup = await setupMint(connection);
-
-    const program = anchor.workspace.Intersolar as anchor.Program<Intersolar>;
-    const [intersolarPublicKey, bump] = await anchor.web3.PublicKey.findProgramAddress(
-      [Buffer.from(PREFIX), setup.mint.publicKey.toBuffer()],
-      program.programId
-    );
-
-    await program.rpc.initialize(
-      bump,
-      {
-        accounts: {
-          intersolar: intersolarPublicKey,
-          user: setup.receiverKeypair.publicKey,
-          tokenMint: setup.mint.publicKey,
-          systemProgram: anchor.web3.SystemProgram.programId
-        },
-        signers: [
-          setup.receiverKeypair,
-        ]
-      });
+    await setupIntersolar(connection);
   });
 });
